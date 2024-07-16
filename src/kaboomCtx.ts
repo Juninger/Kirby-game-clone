69019e26d8dcaabf7334acc65c8b56e13e5496ca
Juninger <marcus.juninger@gmail.com>
2024-07-16 14:58:56 +0200

    Update kaboomCtx.ts
    Initialized Kaboom export with detailed props

--- src/kaboomCtx.ts
+++ src/kaboomCtx.ts
@@ -1,1 +1,13 @@
 // used to initialize the Kaboom.js library and export its context to be used elsewhere
+
+import kaboom from "kaboom";
+import { scale } from "./constants";
+
+export const k = kaboom({
+    // gameboy resolution adapted for 16:9, see comment in constants.ts for 'scale' usage
+    width: 256 * scale,
+    height: 144 * scale,
+    scale,
+    letterbox: true,
+    global: false,
+});
