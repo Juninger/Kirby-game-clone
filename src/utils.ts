244e18173d8bec3a135ae9509c2873e2b32ab7e1
Juninger <marcus.juninger@gmail.com>
2024-07-18 15:36:13 +0200

    Update utils.ts
    Implemented logic for reading level details from JSON and building the map object.

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,64 @@
 // map-related utility
+
+import { KaboomCtx } from 'kaboom';
+import { scale } from './constants';
+
+// name --> name of the map
+export async function makeMap(k: KaboomCtx, name: string) {
+    // read the mapdata from associated json file
+    const mapData = await (await fetch(`./${name}.json`)).json();
+
+    // create a map object without adding it to the map
+    const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
+
+    // store locations of spawn points for both player and enemies
+    const spawnPoints: { [key: string]: { x: number; y: number; }[] } = {}; // e.g. key can be "player" / "flame" / "bird" / "guy" to find the spawn location of that unit
+
+    // take the mapdata object and iterate on its layers
+    for (const layer of mapData.layers) {
+
+        // check what type of layer it is
+        if (layer.name === "colliders") {
+
+            // iterate collider objects and add them to the map
+            for (const collider of layer.objects) {
+                map.add([
+                    k.area({
+                        // shape of the collider, fetches sizes from collider created with Tiled
+                        shape: new k.Rect(k.vec2(0), collider.width, collider.height),
+
+                        // filter which objects should have collisionIgnore enabled
+                        collisionIgnore: ["platform", "exit"], 
+                    }),
+
+                    // if the object is not the exit, set static property of body to true to make it not move on collision
+                    collider.name !== "exit" ? k.body({isStatic: true}) : null,
+
+                    // position of collider
+                    k.pos(collider.x, collider.y), 
+
+                    // if the object being added tot he map is NOT the exit, we can give it the platform tag
+                    collider.name !== "exit" ? "platform" : "exit"
+                ]);
+            }
+            continue;
+        }
+
+        if (layer.name === "spawnpoints") {
+            for (const spawnPoint of layer.objects) {
+                // check if the KEY exists in the spawnpoints array already
+                if (spawnPoints[spawnPoint.name]) {
+                    // add spawnpoint to array using the character's name and position for key-value
+                    spawnPoints[spawnPoint.name].push({ 
+                        x: spawnPoint.x,
+                        y: spawnPoint.y,
+                    });
+                    continue;
+                } 
+                // if key did not exist, create new array and insert spawnpoint instead
+                spawnPoints[spawnPoint.name] = [{ x: spawnPoint.x, y: spawnPoint.y }];
+            }
+        }
+        return { map, spawnPoints };
+    }
+}
