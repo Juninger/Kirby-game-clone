55a2cd1940d27d7376f5587cf722f15fd1c52961
Juninger <marcus.juninger@gmail.com>
2024-07-23 19:33:12 +0200

    Update main.ts
    Added the player object to the game + adjusted the camera position and movement

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // entry point of the project
 
+import { makePlayer } from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -42,6 +43,25 @@ async function gameSetup() {
 
         // add layout to the game
         k.add(level1Layout);
+
+        // create player object
+        const player = makePlayer(
+            k,
+            level1SpawnPoints.player[0].x,
+            level1SpawnPoints.player[0].y
+        );
+
+        // add player to the game
+        k.add(player);
+
+        // camera adjustments
+        k.camScale(0.7, 0.7);
+        k.onUpdate(() => { // event that runs every frame (~60 times per second)
+            // make the camera follow the player
+            if (player.pos.x < level1Layout.pos.x + 432) { 
+                k.camPos(player.pos.x + 500, 800); // adjust camera so that the player appears on the left side of the screen at height 800
+            }
+        });
     });
 
     k.go("level-1");
